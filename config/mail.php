3c62857f09d57a2a4da1f67c2ba6d6b65caba1f3
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2024-06-16 23:45:14 +0700

    ops: change mail config for allow authentication mail

--- config/mail.php
+++ config/mail.php
@@ -41,11 +41,13 @@ return [
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption' => env('MAIL_ENCRYPTION', ''),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'auth_mode'  => null,
+            'verify_peer'=> false,
         ],
 
         'ses' => [
