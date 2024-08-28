8be6f5bfb28b53fde4031c25c062e054dd0f4f0e
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2024-08-29 00:02:54 +0700

    fix: change max file upload

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -10,7 +10,7 @@ class UploadController extends Controller
   public function image(Request $request)
   {
     $request->validate([
-      'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+      'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:10240',
     ]);
 
     $image = $request->file('image');
